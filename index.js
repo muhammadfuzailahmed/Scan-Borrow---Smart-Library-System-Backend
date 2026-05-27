acc6a9bfacf6336d011ffc50d16e508a6069a0d5
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-05-27 20:01:02 +0500

    feat(index): add function to connect database

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import dotenv from "dotenv"
+import express from "express"
+import { connectDB } from "./DB/db.js";
 
 dotenv.config({
     path: "./.env"
 })
 
-console.log("Hello world!, running on port: ", process.env.PORT)
+const app = express();
+
+app.use(express.json())
+
+
+console.log("Hello world!, running on port:", process.env.PORT)
+
+connectDB().then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log("listening on http://localhost:",process.env.PORT)
+    })
+}).catch(() => {
+    console.log("error")
+})
+
