7568d274cac0fa30a3fde3b7d306d52f4791c145
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-06-07 00:14:01 +0500

    feat(student route): add route to return book

--- routes/student.route.js
+++ routes/student.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getStudentDashboardData, borrowBook, getBorrowedBooksByCurrentUser, getCurrentUserBorrowingHistory} from "../Controller/student.controller.js";
+import { getStudentDashboardData, borrowBook, getBorrowedBooksByCurrentUser, getCurrentUserBorrowingHistory, returnBook} from "../Controller/student.controller.js";
 
 const router = Router();
 
@@ -7,5 +7,6 @@ router.route("/student-dashboard-data/:userId").get(getStudentDashboardData)
 router.route("/borrow").post(borrowBook)
 router.route("/borrowedBooks/:userId").get(getBorrowedBooksByCurrentUser)
 router.route("/borrowedBooksHistory/:userId").get(getCurrentUserBorrowingHistory)
+router.route("/returnBook").post(returnBook)
 
 export default router
