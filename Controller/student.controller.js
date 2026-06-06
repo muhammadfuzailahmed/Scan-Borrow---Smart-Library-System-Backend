690461c96fb4f7ce5604d836c3cfd7476dae4e3e
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-06-07 00:13:20 +0500

    feat(student controller): add functionality to return book

--- Controller/student.controller.js
+++ Controller/student.controller.js
@@ -46,17 +46,18 @@ const getStudentDashboardData = async (req, res) => {
 const borrowBook = async (req, res) => {
   const { userId, QRcode } = req.body;
 
-  const numberOfBorrowedBooksResult = await sql.query`select count(*) AS totalBorrowedBooks from transaction_records where userId = ${userId} and book_status = 'ISSUED'`
+  const numberOfBorrowedBooksResult =
+    await sql.query`select count(*) AS totalBorrowedBooks from transaction_records where userId = ${userId} and book_status = 'ISSUED'`;
 
-  const numberOfBorrowedBooks = numberOfBorrowedBooksResult.recordset[0].totalBorrowedBooks;
+  const numberOfBorrowedBooks =
+    numberOfBorrowedBooksResult.recordset[0].totalBorrowedBooks;
 
-  if(numberOfBorrowedBooks >= 3) {
+  if (numberOfBorrowedBooks >= 3) {
     return res.status(409).json({
       success: false,
-      message: "You have reached maximum limit to borrow books."
-    })
+      message: "You have reached maximum limit to borrow books.",
+    });
   }
-
 
   const userResult =
     await sql.query`select * from users where userId = ${userId}`;
@@ -77,7 +78,8 @@ const borrowBook = async (req, res) => {
     });
   }
 
-  const bookCopyIdResult = await sql.query`select bookCopyId from book_copies where QRcode = ${QRcode}`
+  const bookCopyIdResult =
+    await sql.query`select bookCopyId from book_copies where QRcode = ${QRcode}`;
   const bookCopyId = bookCopyIdResult.recordset[0].bookCopyId;
 
   const QRcodeResult =
@@ -130,63 +132,67 @@ const borrowBook = async (req, res) => {
 
   const transactionCode = generateTransactionCode();
 
-
   const issueBookToUser =
     await sql.query`insert into transaction_records (transactionCode, bookCopyId, userId, issueDate, dueDate, returnDate, book_status) values ( ${transactionCode}, ${bookCopy.bookCopyId}, ${userId}, ${formattedIssueDate}, ${formattedDueDate}, null, 'ISSUED')`;
 
-  const updateIsIssued = await sql.query`update book_copies set isIssued = 1 where bookCopyId = ${bookCopyId}`
+  const updateIsIssued =
+    await sql.query`update book_copies set isIssued = 1 where bookCopyId = ${bookCopyId}`;
 
-  const borrowedBookData = await sql.query`select * from transaction_records where userId = ${userId} and bookCopyId = ${bookCopyId}`
+  const borrowedBookData =
+    await sql.query`select * from transaction_records where userId = ${userId} and bookCopyId = ${bookCopyId}`;
 
-  if(borrowedBookData.recordset.length === 0){
+  if (borrowedBookData.recordset.length === 0) {
     return res.status(402).json({
       success: false,
-      message: "Error fetching borrowed book data"
-    })
+      message: "Error fetching borrowed book data",
+    });
   }
 
-  const getBorrowedBookCopyCodeResult = await sql.query`select copyCode from book_copies where bookCopyId = ${bookCopyId}`
+  const getBorrowedBookCopyCodeResult =
+    await sql.query`select copyCode from book_copies where bookCopyId = ${bookCopyId}`;
 
-  if(getBorrowedBookCopyCodeResult.recordset.length === 0) {
-     return res.status(403).json({
+  if (getBorrowedBookCopyCodeResult.recordset.length === 0) {
+    return res.status(403).json({
       success: false,
-      message: "Error getting Book Copy Code"
-     }) 
+      message: "Error getting Book Copy Code",
+    });
   }
 
-  const getBorrowedBookCopyCode = getBorrowedBookCopyCodeResult.recordset[0].copyCode;
+  const getBorrowedBookCopyCode =
+    getBorrowedBookCopyCodeResult.recordset[0].copyCode;
 
-     const bookNameResult = await sql.query`select bookName from books where bookId = (select bookId from book_copies where bookCopyId = ${bookCopyId})`
+  const bookNameResult =
+    await sql.query`select bookName from books where bookId = (select bookId from book_copies where bookCopyId = ${bookCopyId})`;
 
-     if(bookNameResult.recordset.length === 0) {
-      return res.status(403).json({
+  if (bookNameResult.recordset.length === 0) {
+    return res.status(403).json({
       success: false,
-      message: "Error getting book name"
-     }) 
-     }
+      message: "Error getting book name",
+    });
+  }
 
-     const bookName = bookNameResult.recordset[0].bookName;
-
+  const bookName = bookNameResult.recordset[0].bookName;
 
   return res.status(200).json({
     success: true,
     message: "Book Issued Successfully!",
     borrowedBook: borrowedBookData.recordset[0],
     bookCopyCode: getBorrowedBookCopyCode,
-    bookName: bookName
+    bookName: bookName,
   });
 };
 
 const getBorrowedBooksByCurrentUser = async (req, res) => {
-  const {userId} = req.params;
+  const { userId } = req.params;
 
-  const getBooksQueryResult = await sql.query`select b.bookName, bc.copyCode, tr.transactionCode, tr.issueDate, tr.dueDate, tr.book_status AS status from transaction_records tr JOIN book_copies bc ON tr.bookCopyId = bc.bookCopyId JOIN books b ON bc.bookId = b.bookId where tr.book_status = 'ISSUED' and tr.userId = ${userId}`
+  const getBooksQueryResult =
+    await sql.query`select b.bookName, bc.copyCode, tr.transactionCode, tr.issueDate, tr.dueDate, tr.book_status AS status from transaction_records tr JOIN book_copies bc ON tr.bookCopyId = bc.bookCopyId JOIN books b ON bc.bookId = b.bookId where tr.book_status = 'ISSUED' and tr.userId = ${userId}`;
 
-  if(getBooksQueryResult.recordset.length === 0) {
+  if (getBooksQueryResult.recordset.length === 0) {
     return res.status(201).json({
       success: true,
-      message: "Currently you have no books borrowed"
-    })
+      message: "Currently you have no books borrowed",
+    });
   }
 
   const getBooksQuery = getBooksQueryResult.recordset;
@@ -194,20 +200,21 @@ const getBorrowedBooksByCurrentUser = async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Borrowed books fetched successfully!",
-    issuedBooks: getBooksQuery
-  })
-}
+    issuedBooks: getBooksQuery,
+  });
+};
 
 const getCurrentUserBorrowingHistory = async (req, res) => {
-  const {userId} = req.params;
+  const { userId } = req.params;
 
-  const getBooksQueryResult = await sql.query`select b.bookName, bc.copyCode, tr.transactionCode, tr.issueDate, tr.dueDate, tr.book_status AS status from transaction_records tr JOIN book_copies bc ON tr.bookCopyId = bc.bookCopyId JOIN books b ON bc.bookId = b.bookId where tr.userId = ${userId}`
+  const getBooksQueryResult =
+    await sql.query`select b.bookName, bc.copyCode, tr.transactionCode, tr.issueDate, tr.dueDate, tr.book_status AS status from transaction_records tr JOIN book_copies bc ON tr.bookCopyId = bc.bookCopyId JOIN books b ON bc.bookId = b.bookId where tr.userId = ${userId}`;
 
-  if(getBooksQueryResult.recordset.length === 0) {
+  if (getBooksQueryResult.recordset.length === 0) {
     return res.status(201).json({
       success: true,
-      message: "Currently you have no books borrowed"
-    })
+      message: "Currently you have no books borrowed",
+    });
   }
 
   const getBooksQuery = getBooksQueryResult.recordset;
@@ -215,9 +222,42 @@ const getCurrentUserBorrowingHistory = async (req, res) => {
   res.status(200).json({
     success: true,
     message: "Borrowed books fetched successfully!",
-    borrowedBooksHistory: getBooksQuery
-  })
+    borrowedBooksHistory: getBooksQuery,
+  });
 
-}
+};
 
-export {getStudentDashboardData, borrowBook, getBorrowedBooksByCurrentUser, getCurrentUserBorrowingHistory}
+ const returnBook = async (req, res) => {
+    const { transactionCode, userId } = req.body;
+
+    const bookCopyIdResult =
+      await sql.query`select bookCopyId from transaction_records where transactionCode = ${transactionCode}`;
+    const bookCopyId = bookCopyIdResult.recordset[0].bookCopyId;
+
+    await sql.query`update book_copies set isIssued = 0 where bookCopyId = ${bookCopyId}`;
+
+    const date = new Date();
+
+    const formattedDate = date.toLocaleDateString("en-GB", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+
+    await sql.query`update transaction_records set book_status = 'RETURNED' where bookCopyId = ${bookCopyId}`;
+    await sql.query`update transaction_records set returnDate = ${formattedDate} where bookCopyId = ${bookCopyId}`;
+
+    return res.status(200).json({
+      success: true,
+      message: "Book returned successfully!"
+    })
+
+  };
+
+export {
+  getStudentDashboardData,
+  borrowBook,
+  getBorrowedBooksByCurrentUser,
+  getCurrentUserBorrowingHistory,
+  returnBook
+};
