a0fad5354acdbb418f4d4e026cbc6526f9c07829
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-06-16 22:05:15 +0500

    feat(routes - auth): add route for logout

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { loginUser, getCurrentUser } from "../Controller/auth.controller.js";
+import { loginUser, getCurrentUser, logoutUser } from "../Controller/auth.controller.js";
 
 const router = Router();
 
 router.route("/login").post(loginUser)
 router.route("/current-user/:userId").get(getCurrentUser)
+router.route("/logout").post(logoutUser)
 
 export default router
