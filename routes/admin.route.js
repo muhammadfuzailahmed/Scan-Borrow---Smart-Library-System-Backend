e99b19d8874d51c0b490ef87e49b4feaf6696bc5
Muhammad Fuzail Ahmed <fuzailahmed30jan@gmail.com>
2026-06-18 22:33:26 +0500

    feat(routes - admin): add route for add book, update book info and delete book functionality

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getAdminDashboardData, getTransactionsPageData, getBookDetails, bookCopiesDetails, getAdminRecordsPageData } from "../Controller/admin.controller.js";
+import { getAdminDashboardData, getTransactionsPageData, getBookDetails, bookCopiesDetails, getAdminRecordsPageData, addBook, addBookCopy, updateBookInfo, deleteBook } from "../Controller/admin.controller.js";
 import verifyJWT from "../Middleware/verifyjwt.js";
 import { verifyAdmin } from "../Middleware/verifyRole.js";
 
@@ -10,5 +10,9 @@ router.route("/admin-transactions-data").get(verifyJWT, verifyAdmin, getTransact
 router.route("/admin-book-details").get(verifyJWT, verifyAdmin, getBookDetails)
 router.route("/admin-book-copies-details").get(verifyJWT, verifyAdmin, bookCopiesDetails)
 router.route("/admin-records").get(verifyJWT, verifyAdmin, getAdminRecordsPageData)
+router.route("/add-book").post(verifyJWT, verifyAdmin, addBook)
+router.route("/add-book-copy").post(verifyJWT, verifyAdmin, addBookCopy)
+router.route("/update-book-info").post(verifyJWT, verifyAdmin, updateBookInfo)
+router.route("/delete-book").post(verifyJWT, verifyAdmin, deleteBook)
 
 export default router
